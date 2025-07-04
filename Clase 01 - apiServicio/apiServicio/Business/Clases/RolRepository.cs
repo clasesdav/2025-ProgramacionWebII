3ff654d57318a48bf4a1b21f789b5df961f9c821
Docente-Lab <davidserrudoclases@gmail.com>
2025-07-03 21:14:03 -0400

    se sube el proyecto de servicio

---
+++ Clase 01 - apiServicio/apiServicio/Business/Clases/RolRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+using apiServicio.Business.Contracts;
+using apiServicio.Models;
+
+namespace apiServicio.Business.Clases
+{
+    public class RolRepository : IRolRepository
+    {
+        private readonly string connec;
+        public RolRepository(IConfiguration configuration)
+        {
+            connec = configuration.GetConnectionString("conBase");
+        }
+
+        public async Task<List<Rol>> GetList()
+        {
+            List<Rol> list = new List<Rol>();
+            Rol l;
+            using(SqlConnection conn = new SqlConnection(connec))
+            {
+                await conn.OpenAsync();
+                SqlCommand cmd = new SqlCommand("SELECT *  FROM Rol;", conn);
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        l = new Rol();
+                        l.Id = Convert.ToInt32(reader["Id"]);
+                        l.NombreRol= Convert.ToString(reader["NombreRol"]);
+                        list.Add(l);
+                    }
+                }
+            }
+
+
+            return list;
+        }
+
+    }
+}
